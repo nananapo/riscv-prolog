eec06cf7cf4b9cba543b36ad4faf5f5af3caab82
nananapo <kanapodream@gmail.com>
2022-12-01 23:52:48 +0900

    sub命令を実装

--- cpu.pl
+++ cpu.pl
@@ -57,19 +57,23 @@ id_rs_value(Regs, [R0,R1,R2,R3,R4], R_addr, Value) :-
 	bin2decimal([R0,R1,R2,R3,R4], R_addr),
 	getreg(Regs, R_addr, Value).
 
+execute_inst(Inst, Regs, NextRegs) :-
+	inst_add(Inst, Regs, NextRegs);
+	inst_sub(Inst, Regs, NextRegs).
+
 execute(Inst, _, Rip, NextRip, Regs, NextRegs) :-
 	Rip >= 0,
 	write("Execute Stage--"),nl,
 	NextRip is Rip + 32,
-	inst_add(Inst, Regs, NextRegs).
+	execute_inst(Inst, Regs, NextRegs).
 
 inst_add([
 		0,0,0,0,0,
 		0,0,
-		Rs2_0,Rs2_1,Rs2_2,Rs2_3,Rs2_4,
-		Rs1_0,Rs1_1,Rs1_2,Rs1_3,Rs1_4,
+		Rs2_4,Rs2_3,Rs2_2,Rs2_1,Rs2_0,
+		Rs1_4,Rs1_3,Rs1_2,Rs1_1,Rs1_0,
 		0,0,0,
-		Rd_0,Rd_1,Rd_2,Rd_3,Rd_4,
+		Rd_4,Rd_3,Rd_2,Rd_1,Rd_0,
 		0,1,1,0,0,
 		1,1], Regs, RegsNew) :-
 	id_rs_value(Regs, [Rs2_0,Rs2_1,Rs2_2,Rs2_3,Rs2_4], Rs2_addr, Rs2_data),
@@ -77,6 +81,24 @@ inst_add([
 	id_rs_value(Regs, [Rd_0,Rd_1,Rd_2,Rd_3,Rd_4], Rd_addr, _),
 	Result is Rs1_data + Rs2_data,
 	write("add(rs2,rs1,rd) : "),write(Rs2_addr),write(","),write(Rs1_addr),write(","),write(Rd_addr),nl,
+	write(Rs1_data),write(","),write(Rs2_data),write(","),write(Rd_addr),nl,
+	setreg(Regs, Rd_addr, Result, RegsNew).
+
+inst_sub([
+		0,1,0,0,0,
+		0,0,
+		Rs2_4,Rs2_3,Rs2_2,Rs2_1,Rs2_0,
+		Rs1_4,Rs1_3,Rs1_2,Rs1_1,Rs1_0,
+		0,0,0,
+		Rd_4,Rd_3,Rd_2,Rd_1,Rd_0,
+		0,1,1,0,0,
+		1,1], Regs, RegsNew) :-
+	id_rs_value(Regs, [Rs2_0,Rs2_1,Rs2_2,Rs2_3,Rs2_4], Rs2_addr, Rs2_data),
+	id_rs_value(Regs, [Rs1_0,Rs1_1,Rs1_2,Rs1_3,Rs1_4], Rs1_addr, Rs1_data),
+	id_rs_value(Regs, [Rd_0,Rd_1,Rd_2,Rd_3,Rd_4], Rd_addr, _),
+	Result is Rs1_data - Rs2_data,
+	write("sub(rs2,rs1,rd) : "),write(Rs2_addr),write(","),write(Rs1_addr),write(","),write(Rd_addr),nl,
+	write(Rs1_data),write(","),write(Rs2_data),write(","),write(Rd_addr),nl,
 	setreg(Regs, Rd_addr, Result, RegsNew).
 
 #riscv(_, 32, _) :- write("end"), nl.
